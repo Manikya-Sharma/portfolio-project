170cf79832f02e4d37212f580af915e0c87e004a
Ritvik <kumarritvik.astro04@gmail.com>
2023-04-16 13:30:54 +0530

    Mentorship chenges

--- mentorship-code.js
+++ mentorship-code.js
@@ -276,3 +276,20 @@ setInterval(() => {
     t4.classList.add("hidden");
   }
 }, 5500);
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
