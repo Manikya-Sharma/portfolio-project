76398eb07ebc2bb2f9caf23026179a4b961486fa
Ritvik <kumarritvik.astro04@gmail.com>
2023-04-16 12:41:22 +0530

    Made some improvements

--- mentorship-code.js
+++ mentorship-code.js
@@ -242,3 +242,20 @@ setInterval(() => {
     t4.classList.add("hidden");
   }
 }, 3500);
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
